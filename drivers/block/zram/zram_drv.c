31cdfc3604b91e9263ad85586b17a97d6c63a3e1
Wys-Carzy <505742305@qq.com>
2020-12-29 15:02:20 +0800

    change zram size to 4096

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1013,7 +1013,7 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = (u64)SZ_1M * 8192;
+	disksize = (u64)SZ_1M * 4096;
 	if (!disksize)
 		return -EINVAL;
 
